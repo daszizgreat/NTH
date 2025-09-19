1c36faab905629964602b33403bd7d5dd98decf2
Soumyadeep das <120002786+daszizgreat@users.noreply.github.com>
2025-09-19 21:39:18 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pymongo
+
 from datetime import date
 import base64
 import os
@@ -11,7 +12,7 @@ def hide_sidebar():
         <style> [data-testid="stSidebar"] { display: none; } </style>
     """, unsafe_allow_html=True)
 
-hide_sidebar()
+##hide_sidebar()
 # --- PAGE CONFIGURATION ---
 st.set_page_config(
     page_title="Centralized Registration",
@@ -203,5 +204,4 @@ else:
 
     if st.button("🔑 Proceed to Login", use_container_width=True):
         st.switch_page("pages/1_login.py")
-
 
